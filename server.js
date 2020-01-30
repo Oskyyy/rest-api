bbd5e8c052c16cbe7b8fd0e29abc3d50984eadd1
Oskyyy <oskarwro@gmail.com>
2020-01-30 21:30:17 +0100

    Add endpoints

--- server.js
+++ server.js
@@ -1,21 +1,63 @@
 const express = require('express');
 
-// create server
 const app = express();
 
-// middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
 
-// database
-const db = [
+let db = [
   { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
   { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
 ];
 
+app.get('/testimonials', (req, res) => {
+  res.json(db);
+});
 
-// http port
+app.get('/testimonials/:id', (req, res) => {
+  let newDb = db.filter((item) => {
+    return item.id == req.params.id;
+  })
+  res.json(newDb);
+});
+
+app.get('/testimonials/random', (req, res) => {
+
+});
+
+app.post('/testimonials/', (req, res) => {
+  const { author, text } = req.body;
+  db.push({id: (db[db.length -1].id +1) ,author, text});
+  res.send({ message: 'OK' });
+});
+
+app.put('/testimonials/:id', (req, res) => {
+  const { author, text } = req.body;  
+  const { id } = req.params;
+
+  db.map((item) => {
+    if(item.id == id){
+      item.author = author;
+      item.text = text;
+      return item;
+    }
+    return item;
+  });
+  res.send({ message: 'OK' }); 
+});
+
+app.delete('/testimonials/:id', (req, res) => {
+  const {id} = req.params;
+  db = db.filter((item) => {
+    return item.id != id;
+  })
+  res.send({ message: 'OK' });
+});
+
+app.use((req, res, next) => {
+  res.send({ message: 'Not found' });
+});
+
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
