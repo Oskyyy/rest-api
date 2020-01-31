8346a486b1d38901996ec80283923d48c69f69ef
Oskar Wrotniak <oskarwro@gmail.com>
2020-01-31 15:37:43 +0100

    deploying on heroku in progress

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
@@ -15,13 +16,19 @@ app.use(
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); 
 app.use('/api', testimonialsRoutes);
-app.use('/api', concertsRoutes); 
+app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
 app.use((req, res, next) => {
   res.send({ message: 'Not found' });
 });
 
-app.listen(8000, () => {
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
