d857bca58514223def3d2a33570cc33a520ab76b
Oskyyy <oskarwro@gmail.com>
2020-02-10 21:11:28 +0100

    Add helmet

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
+const cors = require('cors');
 const socket = require('socket.io');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+
 const app = express();
-
-const concertsRoutes = require('./routes/concerts.routes');
-const seatsRoutes = require('./routes/seats.routes');
-const testimonialsRoutes = require('./routes/testimonials.routes');
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
+app.use(helmet());
+
+app.use(
+  cors({ origin: 'http://localhost:3000', methods: 'GET, POST, PUT, DELETE' })
+);
 
 app.use((req, res, next) => {
   req.io = io;
@@ -20,33 +21,46 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, '/client/build')));
 
-app.use('/api', concertsRoutes);
-app.use('/api', seatsRoutes);
+const testimonialsRoutes = require('./routes/testimonials.routes');
+const seatsRoutes = require('./routes/seats.routes');
+const concertsRoutes = require('./routes/concert.routes');
+
 app.use('/api', testimonialsRoutes);
+app.use('/api', seatsRoutes);
+app.use('/api', concertsRoutes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 app.use((req, res) => {
-  res.status(404).send('404 not found...');
-})
+  res.status(404).send({ message: '404 not found...' });
+});
 
-mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true, useUnifiedTopology: true});
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is runnng on port: 8000');
+  console.log('Start working NOW!!');
+});
+
+const cluster =
+  'mongodb+srv://shivetay:Chi7i!13@cluster0-h60yd.mongodb.net/NewWaveDB';
+
+mongoose.connect(cluster, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 
 db.once('open', () => {
   console.log('Connected to the database');
 });
+db.on('error', err => console.log('Error ' + err));
 
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
+const io = socket(server);
+
+io.on('connection', socket => {
+  console.log('client with ID:', socket.id, ' has just logged');
+  socket.on('disconnect', () => {
+    console.log('client ID: ', socket.id, ' has just left');
+  });
 });
-
-const io = socket(server)
-
-io.on('connection', (socket) => {
-  console.log('New socket!');
-});
-
-module.exports = server;
